56734a4b0b265aa1e5f2373036d765482de00b55
70NY-L1 <47357967+70NY-L1@users.noreply.github.com>
2019-02-07 00:24:49 +0800

    read file

--- myftp/myftp.c
+++ myftp/myftp.c
@@ -10,15 +10,23 @@
 
 int create_directory;
 DIR *dir;
+struct dirent *dp;
+char * file_name;
 int main ()
 {
-    create_directory = mkdir("/data/", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    create_directory = mkdir("./data", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
     if (create_directory != 0)
     {
         printf("Error in creating directory!");
     }
     
-    dir = opendir("/data");
+    dir = opendir("./data");
+    while ((dp = readdir(dir)) != NULL)
+    {
+        printf("filename: %s", dp->d_name);
+    }
+    
+
     
     readdir(dir);
 }
