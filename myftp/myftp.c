45b5e9436bb73c66407428c5868da780206a3913
70NY-L1 <47357967+70NY-L1@users.noreply.github.com>
2019-02-07 23:18:59 +0800

    list files

--- myftp/myftp.c
+++ myftp/myftp.c
@@ -14,22 +14,38 @@ DIR *dir;
 struct dirent *dp;
 char * file_name;
 
-int main ()
+void* list_files ()
 {
+    
     create_directory = mkdir("./data", S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
     if (create_directory != 0)
     {
-        printf("Error in creating directory!");
+        printf("Error in creating directory or directory already exists!\n");
     }
     
     dir = opendir("./data");
+    
     while ((dp = readdir(dir)) != NULL)
     {
-        printf("filename: %s", dp->d_name);
+        if (strcmp(dp->d_name, ".") == 1 && strcmp(dp->d_name, "..") == 1)
+        {
+            printf("%s\n", dp->d_name);
+            //return dp->d_name;
+        }
+        
     }
-    
     closedir(dir);
+    return NULL;
+    
 }
+
+int main ()
+{
+    list_files();
+}
+
+
+
 
 
 
