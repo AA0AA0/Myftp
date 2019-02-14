534dffb4f7796bb5749d4fc51504239780d9d299
Mo Mo <MoMo@MoBook-Pro.local>
2019-02-14 13:09:09 +0800

    The makefile show now work for the multithread ver. Some bugs on get occurs now (large file).

--- myftp/myftpserver.c
+++ myftp/myftpserver.c
@@ -1,5 +1,5 @@
 //
-//  myftpserver.c
+//  Test_multithread.c
 //  myftp
 //
 
@@ -16,19 +16,201 @@
 # include <arpa/inet.h>
 # include <sys/stat.h>
 # include <dirent.h>
+# include <pthread.h>
+# include <assert.h>
 # include <sys/uio.h>
 
-int main(int argc, char** argv){
-    int sd=socket(AF_INET,SOCK_STREAM,0);
+void *pthread_prog(void *sDescriptor)
+{
+  	int client_sd = *(int *)sDescriptor;
+  	struct message_s recv_message;
+    unsigned char temp[5] = "myftp";
+    memset(&recv_message,0,sizeof(recv_message));
+    int len;
+    if((len=recv(client_sd,(char*)&recv_message,sizeof(recv_message),0))<0)
+    {
+        printf("receive error: %s (Errno:%d)\n", strerror(errno),errno);
+        free(sDescriptor);
+        pthread_exit(NULL);
+    }
+    if (memcmp(recv_message.protocol, temp,sizeof(temp)) != 0) 
+    {
+        printf("wrong protocol\n");
+        free(sDescriptor);
+        pthread_exit(NULL);
+    }
+    else
+    {
+        //printf("protocol ok\n");
+        //printf("%s\n",recv_message.protocol);
+        //printf("%u\n",recv_message.type);
+        //printf("%u\n",recv_message.length);
+    }
+    //printf("halo\n");
+    struct message_s reply_message;
+    memset(&reply_message, 0, sizeof(reply_message));
+    char reply_payload[1024] = "";
+    if (recv_message.type == 0xA1){
+        list_files(reply_payload);
+        reply_message.length = 10 + strlen(reply_payload);
+        memcpy(reply_message.protocol, temp, sizeof(temp));
+        reply_message.type = 0xA2;
+        if ((len = send(client_sd,(const char *)&reply_message, sizeof(reply_message), 0))< 0) {
+            printf("Error in sending reply message\n");
+            free(sDescriptor);
+            pthread_exit(NULL);
+        }
+        if((len = send(client_sd, reply_payload, sizeof(reply_payload), 0)) < 0){
+            printf("error in sending payload\n");
+        }
+        free(sDescriptor);
+        pthread_exit(NULL);
+    }
+    if (recv_message.type == 0xB1) {
+        int filename_size;
+        filename_size = recv_message.length - 10;
+        char *file;
+        file = (char *)malloc(filename_size * sizeof(char));
+        memset(file,0,(filename_size * sizeof(char)));        //key changes
+        //print_bytes(file, sizeof(file) + 10);
+        if((len=recv(client_sd,file,filename_size,0))<0){
+            printf("receive error: %s (Errno:%d)\n", strerror(errno),errno);
+            free(sDescriptor);
+            pthread_exit(NULL);
+        }
+        char* file_name = (char *)malloc((7+ filename_size) * sizeof(char));
+        strcpy(file_name, "./data/");
+        strcat(file_name, file);
+        //printf("%s\n",file);
+        //print_bytes(file, sizeof(file) + 10);
+        //printf("%d\n",filename_size);
+        if (find_files(file, 0) != 1){
+            reply_message.length = 10;
+            memcpy(reply_message.protocol, temp, 5);
+            reply_message.type = 0xB3;
+            if ((len = send(client_sd, (const char *)&reply_message, sizeof(reply_message), 0))< 0) {
+                printf("Error in sending reply message\n");
+                free(sDescriptor);
+                pthread_exit(NULL);
+            }
+            free(sDescriptor);
+            pthread_exit(NULL);
+        }
+        else {
+            int file_desc, file_size, fs_block_sz;
+            char buff[512];
+            struct stat obj;
+            stat(file_name, &obj);
+            file_desc = open(file_name, O_RDONLY);
+            file_size = obj.st_size;
+            reply_message.length = 10;
+            reply_message.type = 0xB2;
+            memcpy(reply_message.protocol, temp, 5);
+            send(client_sd, (const char *)&reply_message, sizeof(reply_message), 0);
+            struct message_s file_header;
+            memset((void *)&file_header, 0, sizeof(file_header));
+            memcpy(file_header.protocol, temp, 5);
+            file_header.type = 0xFF;
+            file_header.length = 10 + file_size;
+            if ((len = send(client_sd, (const char*)&file_header, sizeof(file_header), 0)) < 0) {
+                printf("Cannot send file header\n");
+                free(sDescriptor);
+                pthread_exit(NULL);
+            }
+            //               sendfile(client_sd, file_desc, NULL, file_size);
+            bzero(buff, 512);
+            while ((fs_block_sz = read(file_desc, buff, 512)) > 0) {
+                if ((len = send(client_sd, buff, fs_block_sz, 0)) < 0) {
+                    printf("Error in sending buffer\n");
+                    free(sDescriptor);
+                    pthread_exit(NULL);
+                }
+                bzero(buff, 512);
+            }
+            printf("The file is successfully sent\n");
+            close(file_desc);
+            free(sDescriptor);
+            pthread_exit(NULL);
+        }
+        //get_request();
+    }
+    if (recv_message.type == 0xC1) 
+    {
+
+        reply_message.length = 10;
+        memcpy(reply_message.protocol, temp, 5);
+        reply_message.type = 0xC2;
+        if ((len = send(client_sd, (const char *)&reply_message, sizeof(reply_message), 0))< 0) {
+            printf("Error in sending reply message\n");
+            exit(1);
+        }
+        char payload1[1024] = "";
+        char buff[512];
+        bzero(buff, 512);
+        int file_desc;
+        if ((len = recv(client_sd, (const char *)&payload1, sizeof(payload1), 0))<0) {
+            printf("Cannot recv server reply");
+            exit(0);
+        }
+        
+        char payload[1024] = "./data/";
+        strcat(payload, payload1);
+        struct message_s file_data;
+        memset((void *)&file_data, 0, sizeof(file_data));
+        if ((len = recv(client_sd, (const char *)&file_data, sizeof(file_data), 0)) < 0) {
+            printf("Error in recv file data header\n");
+            exit(1);
+        }
+        if (memcmp(recv_message.protocol, temp,sizeof(temp)) != 0) {
+            printf("wrong protocol\n");
+            exit(0);
+        }
+        int size;
+        size = file_data.length - 10;
+        if (size == 0) {
+            printf("The file is empty\n");
+            exit(1);
+        }
+        if ((file_desc = open(payload, O_WRONLY | O_CREAT | O_TRUNC, 0666)) < 0) {
+            printf("Cannot create file");
+            exit(0);
+        }
+        int fr_block_sz = 0;
+        while ((fr_block_sz = recv(client_sd, buff, 512, 0)) > 0) {
+            int write_sz = write(file_desc, buff, fr_block_sz);
+            if (write_sz < fr_block_sz) {
+                printf("File write failed\n");
+                exit(0);
+            }
+            bzero(buff, 512);
+            if (fr_block_sz == 0 || fr_block_sz != 512) {
+                break;
+            }
+            if (fr_block_sz < 0) {
+                printf("Error in recv data\n");
+                exit(0);
+            }
+        }
+        
+        printf("Finished recv file\n");
+        close(file_desc);
+        free(sDescriptor);
+        pthread_exit(NULL);
+            //put_request();
+    }
+    pthread_exit(NULL);
+}
+
+int main(int argc, char** argv)
+{
+	int sd=socket(AF_INET,SOCK_STREAM,0);
     long val = 1;
     if (setsockopt(sd,SOL_SOCKET,SO_REUSEADDR, &val, sizeof(long)) == -1)
     {
         perror("setsocopt");
         exit(1);
     }
-    int client_sd;
     struct sockaddr_in server_addr;
-    struct sockaddr_in client_addr;
     memset(&server_addr,0,sizeof(server_addr));
     server_addr.sin_family=AF_INET;
     server_addr.sin_addr.s_addr=htonl(INADDR_ANY);
@@ -41,176 +223,19 @@ int main(int argc, char** argv){
         printf("listen error: %s (Errno:%d)\n",strerror(errno),errno);
         exit(0);
     }
+    
+    int client_sd;
+    int *new_sock;
+    struct sockaddr_in client_addr;
+    memset(&client_addr,0,sizeof(client_addr));
     int addr_len=sizeof(client_addr);
-    if((client_sd=accept(sd,(struct sockaddr *) &client_addr,&addr_len))<0){
-        printf("accept erro: %s (Errno:%d)\n",strerror(errno),errno);
-        exit(0);
-    }
-    while(1)
-    {
-        struct message_s recv_message;
-        unsigned char temp[5] = "myftp";
-        memset(&recv_message,0,sizeof(recv_message));
-        int len;
-        if((len=recv(client_sd,(char*)&recv_message,sizeof(recv_message),0))<0){
-            printf("receive error: %s (Errno:%d)\n", strerror(errno),errno);
-            exit(0);
-        }
-        if (memcmp(recv_message.protocol, temp,sizeof(temp)) != 0) {
-            printf("wrong protocol\n");
-            printf("%s\n", recv_message.protocol);
-            exit(0);
-        }
-        else
-        {
-            printf("protocol ok\n");
-        }
-        struct message_s reply_message;
-        memset((void *)&reply_message, 0, sizeof(reply_message));
-        char reply_payload[1024] = "";
-        if (recv_message.type == 0xA1){
-            printf("list\n");
-            list_files(reply_payload);
-            reply_message.length = 10 + strlen(reply_payload);
-            memcpy(reply_message.protocol, temp, sizeof(temp));
-            reply_message.type = 0xA2;
-            if ((len = send(client_sd,(const char *)&reply_message, sizeof(reply_message), 0))< 0) {
-                printf("Error in sending reply message\n");
-                exit(0);
-            }
-            if((len = send(client_sd, reply_payload, sizeof(reply_payload), 0)) < 0){
-                printf("error in sending payload\n");
-            }
-            exit(0);
-        }
-        if (recv_message.type == 0xB1) {
-            int filename_size;
-            filename_size = recv_message.length - 10;
-            char *file;
-            file = (char *)malloc(filename_size * sizeof(char));
-            memset(file, 0, sizeof(file));
-            if((len=recv(client_sd,file,filename_size,0))<0){
-                printf("receive error: %s (Errno:%d)\n", strerror(errno),errno);
-                exit(0);
-            }
-            char* file_name = (char *)malloc((7+ filename_size) * sizeof(char));
-            strcpy(file_name, "./data/");
-            strcat(file_name, file);
-            if (find_files(file, 0) != 1){
-             reply_message.length = 10;
-             memcpy(reply_message.protocol, temp, 5);
-             reply_message.type = 0xB3;
-             if ((len = send(client_sd, (const char *)&reply_message, sizeof(reply_message), 0))< 0) {
-                 printf("Error in sending reply message\n");
-                 exit(1);
-                }
-                exit(0);
-             }
-             else {
-                 int file_desc, file_size, fs_block_sz;
-                 char buff[512];
-                 struct stat obj;
-                 stat(file_name, &obj);
-                 file_desc = open(file_name, O_RDONLY);
-                 file_size = obj.st_size;
-                 reply_message.length = 10;
-                 reply_message.type = 0xB2;
-                 memcpy(reply_message.protocol, temp, 5);
-                 send(client_sd, (const char *)&reply_message, sizeof(reply_message), 0);
-                 struct message_s file_header;
-                 memset((void *)&file_header, 0, sizeof(file_header));
-                 memcpy(file_header.protocol, temp, 5);
-                 file_header.type = 0xFF;
-                 file_header.length = 10 + file_size;
-                 if ((len = send(client_sd, (const char*)&file_header, sizeof(file_header), 0)) < 0) {
-                     printf("Cannot send file header\n");
-                     exit(0);
-                 }
-//               sendfile(client_sd, file_desc, NULL, file_size);
-                 bzero(buff, 512);
-                 while ((fs_block_sz = read(file_desc, buff, 512)) > 0) {
-                     if ((len = send(client_sd, buff, fs_block_sz, 0)) < 0) {
-                         printf("Error in sending buffer\n");
-                         exit(0);
-                     }
-                     bzero(buff, 512);
-                 }
-                 printf("The file is successfully sent\n");
-                 close(file_desc);
-                 exit(0);
-             }
-             exit(0);
-             //get_request();
-        }
-        if (recv_message.type == 0xC1) {
-            printf("put");
-            printf("%d",recv_message.length);
-            reply_message.length = 10;
-            memcpy(reply_message.protocol, temp, 5);
-            reply_message.type = 0xC2;
-            if ((len = send(client_sd, (const char *)&reply_message, sizeof(reply_message), 0))< 0) {
-                printf("Error in sending reply message\n");
-                exit(1);
-            }
-            char payload1[1024] = "";
-            char buff[512];
-            bzero(buff, 512);
-            int file_desc;
-            if ((len = recv(client_sd, (const char *)&payload1, sizeof(payload1), 0))<0) {
-                printf("Cannot recv server reply");
-                exit(0);
-            }
-            
-            char payload[1024] = "./data/";
-            strcat(payload, payload1);
-            struct message_s file_data;
-            memset((void *)&file_data, 0, sizeof(file_data));
-            if ((len = recv(client_sd, (const char *)&file_data, sizeof(file_data), 0)) < 0) {
-                printf("Error in recv file data header\n");
-                exit(1);
-            }
-            if (memcmp(recv_message.protocol, temp,sizeof(temp)) != 0) {
-                printf("wrong protocol\n");
-                printf("%s\n", recv_message.protocol);
-                exit(0);
-            }
-            int size;
-            size = file_data.length - 10;
-            if (size == 0) {
-                printf("The file is empty\n");
-                exit(1);
-            }
-            if ((file_desc = open(payload, O_WRONLY | O_CREAT | O_TRUNC, 0666)) < 0) {
-                printf("Cannot create file");
-                exit(0);
-            }
-            int fr_block_sz = 0;
-            while ((fr_block_sz = recv(client_sd, buff, 512, 0)) > 0) {
-                int write_sz = write(file_desc, buff, fr_block_sz);
-                if (write_sz < fr_block_sz) {
-                    printf("File write failed\n");
-                    exit(0);
-                }
-                bzero(buff, 512);
-                if (fr_block_sz == 0 || fr_block_sz != 512) {
-                    break;
-                }
-                if (fr_block_sz < 0) {
-                    printf("Error in recv data\n");
-                    exit(0);
-                }
-            }
-            
-            printf("Finished recv file\n");
-            close(file_desc);
-            exit(0);
-            //put_request();
-        }
-      /*  if(strcmp("exit",buff)==0){
-            close(client_sd);
-            break;
-        }*/
-    }
-    close(sd);
-    return 0;
+	while(client_sd=accept(sd,(struct sockaddr *) &client_addr,&addr_len))
+	{
+		pthread_t worker;
+        new_sock = malloc(1);
+        *new_sock = client_sd;
+        pthread_create(&worker, NULL, pthread_prog, (void*) new_sock);
+        pthread_join(worker,NULL);
+        memset(&client_addr,0,sizeof(client_addr));
+	}
 }
